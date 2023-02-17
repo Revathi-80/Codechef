16cbf8e8ac24f6fd04ae538d3104dfa528c5eb6b
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-02-17 22:11:31 +0530

    Create turn it.cpp

---
+++ 1*-2*/Basic Programming/turn it.cpp
@@ -0,0 +1,68 @@
+//https://www.codechef.com/LP1TO201/problems/NFS
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+//vector<int>pre(1e6+1);
+const int N=1e6 + 5;
+bool prime[N];
+
+void setIO(string s) {
+  freopen((s+".in").c_str(),"r",stdin);
+  freopen((s+".out").c_str(),"w",stdout);
+}
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--) {
+        double u,v,a,s;
+        cin>>u>>v>>a>>s;
+        double speed =(pow(u,2) - 2*a*s);
+       
+        if(speed>v*v)
+        cout<<"No"<<endl;
+        else
+        cout<<"Yes"<<endl;
+    }
+ 
+  return 0;
+ }
+
+/*-----------------------------------------------------------------------------------------------------*/
+//  auto cmp=[](const pair<int,int>&a ,const pair<int,int>&b){
+//   return a.first>b.first;
+// };
+/*--------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------*/
+void sieve(){
+   prime[0]=prime[1]=1;
+   for(int i=2;i<=N;i++){
+    if(!prime[i]) {
+      for(int j=i*i;j<=N;j+=i){
+        prime[j]=1;
+      }
+    }
+   }
+}
+/*-----------------------------------------------------------------*/
+
+void PrimeFactors(int n,vector<int>&ans) {
+   while (n%2 == 0){
+      ans.push_back(2);
+      n = n/2;
+   }
+   for (int i = 3; i <= sqrt(n); i = i+2){
+      while (n%i == 0){
+         ans.push_back(i);
+         n = n/i;
+      }
+   }
+   if (n > 2)
+   ans.push_back(n);
+}
+
+
+
+
+  
